8577eec661fed694db9cc1958c997278dcec69e9
midoriI2026 <106957602+midoriI2026@users.noreply.github.com>
2026-05-09 10:19:49 +0900

    Create timecardck.user.js

---
+++ timecardck.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         TimeCard Checker (出勤・退勤チェック)
+// @namespace    http://tampermonkey.net/
+// @version      1.1
+// @match        https://midorinet-iwate.cybozu.com/o/ag.cgi?page=TimeCardIndex*
+// @match        https://midorinet-iwate.cybozu.com/o/ag.cgi?page=SystemTimeCardUser*
+// @run-at       document-end
+// @require      https://raw.githubusercontent.com/midoriI2026/timecardck/main/timecardck.js
+// @grant        none
+// ==/UserScript==
